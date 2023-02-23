de8480d0148cc8563b224081b6dae632b604affc
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-02-23 09:35:30 +0200

    Updates

---
+++ assets/index-e782fbac.js
@@ -0,0 +1,1 @@
+import{j as o,T as t}from"./index-3a2709a5.js";import{P as e}from"./Page-85942681.js";const n=()=>o(e,{children:o(t,{component:"h2",variant:"h2",textAlign:"center",children:"Not Found Page"})}),s=n;export{s as default};
